70cea5dbbc77e97435dbe713682652db22bbb1b3
darklotuskdb <th.kamalsinghrajput@gmail.com>
2025-05-11 16:42:02 -0400

    Basic improvements: DC

--- magazine/DNS-Check.sh
+++ magazine/DNS-Check.sh
@@ -2,7 +2,7 @@
 
 dnsScan() {
   echo "[*] Running DNS Scan"
-  /opt/dnscan/dnscan.py -d "$tar" -w Master_wordlist.txt -r -L $BASE_DIR/config/dns-resolvers.txt -t 50 -D -o dns-scan-output.txt
+  /opt/dnscan/dnscan.py -d "$tar" -w filtered-Master_wordlist.txt -r -L $BASE_DIR/config/dns-resolvers.txt -t 50 -D -o dns-scan-output.txt
 
   grep "$tar" dns-scan-output.txt | grep -vE "@$tar|\*" | awk '{print $1}' | sed '/^$/d' | sort -u >> dns-scan.txt
   echo -e "\n[+] DNS scan completed. Results saved to dns-scan.txt"
